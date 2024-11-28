eca551ab949cc98047c56e65e21683d3d7504697
guillelahoz <guilleguille394@gmail.com>
2024-11-28 19:55:43 +0100

    Ejercicio 1

---
+++ modulo-02/src/components/CabeceraEjercicio.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="cabecera-ejercicio">
+      <!-- Slot por defecto (titulo para el ejercicio) -->
+      <h1><slot></slot></h1>
+  
+      <!-- Slot para la descripción del ejercicio,(app.vue=#descripcion)-->
+      <p><slot name="descripcion"></slot></p>
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'CabeceraEjercicio',
+  });
+  </script>
+  
+  <style scoped>
+  .cabecera-ejercicio {
+    background-color: #f4f4f4;
+    padding: 20px;
+    border-radius: 8px;
+    margin-bottom: 20px;
+    border: 1px solid #ccc;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  }
+  
+  .cabecera-ejercicio h1 {
+    font-size: 24px;
+    color: #333;
+    margin: 0;
+  }
+  
+  .cabecera-ejercicio p {
+    font-size: 16px;
+    color: #666;
+    margin-top: 10px;
+  }
+  </style>
+  
